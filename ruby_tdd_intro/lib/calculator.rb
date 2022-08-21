2d1c709d0693e525162115635b83b790ec8e9d15
Julz-B <julianb@afrihost.co.za>
2022-08-21 12:00:39 +0200

    forked TOP Rspec repo  Please enter the commit message for your changes. Lines starting

--- ruby_tdd_intro/lib/calculator.rb
+++ ruby_tdd_intro/lib/calculator.rb
@@ -1,11 +1,12 @@
-#lib/calculator.rb
+# lib/calculator.rb
 
+# a calculator to add and multiply
 class Calculator
   def add(*nums)
     nums.sum
   end
 
   def multiply(*nums)
-    nums.inject(:*)        
+    nums.inject(:*)
   end
 end
