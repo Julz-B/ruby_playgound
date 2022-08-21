7aac2791d83bbcd8a2800765feb7cfec2be9910c
Julz-B <julianb@afrihost.co.za>
2022-08-21 08:06:30 +0200

    Corrected indentation issues.

--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -1,28 +1,28 @@
-#spec/calculator_spec.rb
+# spec/calculator_spec.rb
 require './lib/calculator'
 
 describe Calculator do
-    describe "#add" do
-        it "returns the sum of two numbers" do
-            calculator = Calculator.new
-            expect(calculator.add(5, 2)).to eql(7)
-        end
-
-        it "returns the sum of more than to numbers" do 
-            calculator  = Calculator.new
-            expect(calculator.add(2, 5, 7)).to eql(14)
-        end
+  describe '#add' do
+    it 'returns the sum of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
     end
 
-    describe "#multiply" do
-        it "returns the product of two numbers" do
-            calculator = Calculator.new
-            expect(calculator.multiply(2, 3)).to eql(6)
-        end
-        
-        it "return the product of more than two numbers" do
-            calculator = Calculator.new
-            expect(calculator.multiply(2, 3, 4)).to eql(24)
-        end
+    it 'returns the sum of more than to numbers' do 
+      calculator  = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
     end
+  end
+
+  describe '#multiply' do
+    it 'returns the product of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(2, 3)).to eql(6)
+    end
+
+    it 'return the product of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(2, 3, 4)).to eql(24)
+    end
+  end
 end
